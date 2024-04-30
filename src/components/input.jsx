d666eb8b3a1320c867e050fe3dff3aa1ef888a30
jacobwoodworth <woodwo51@msu.edu>
2024-04-30 15:18:13 -0400

    added general styling fixes

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -7,14 +7,12 @@ function InputFunction() {
     const [username, setUserName] = useState("");
     const [isVisible, setIsVisible] = useState(false);
     const [submit, setSubmit] = useState(false);
-    const [showWelcome, setShowWelcome] = useState(true);
    
     const handleSubmit = (event) => {
         event.preventDefault();
         if (username.trim()) {
             setSubmit(true);
             setIsVisible(false);
-            setShowWelcome(true);
         }
     };
 
@@ -24,25 +22,26 @@ function InputFunction() {
 
     return (
         <div>
-             {showWelcome && (
                 <div>
                     <h1 id="textbox">How Bad Is Your Movie Taste?</h1>
                     <h2 id="textbox"> Our movie Artifical Intelligence will disect and roast your horrible taste in film.</h2>
                 </div>
-            )}
-            <button onClick={toggleVisibility}>Get Started</button>
+            {!submit && (
+                <>
+            <button onClick={toggleVisibility}>{isVisible ? "Hide Form" : "Get Started"}</button>
             {isVisible && (
                 <div className="animate-input">
                     <form onSubmit={handleSubmit}>
-                        
                         <input
                             placeholder='Enter your Letterboxd username...'
                             type="text"
                             value={username}
                             onChange={(e) => setUserName(e.target.value)}
-                        />
+                            />
                     </form>
                 </div>
+         )}
+                </>
             )}
             {submit && <Main username={username} />}
         </div>
